c62bea0b34aa13418193f48406813bc5c4b29e14
JustaGlitch <justingraham1108@gmail.com>
2022-07-26 15:04:41 +0100

    Testing errors fixed

--- src/test/java/com/qa/ims/persistence/dao/CustomerDAOTest.java
+++ src/test/java/com/qa/ims/persistence/dao/CustomerDAOTest.java
@@ -1,12 +1,9 @@
 package com.qa.ims.persistence.dao;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.junit.Before;
-import org.junit.Test;
 
 import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.utils.DBUtils;
