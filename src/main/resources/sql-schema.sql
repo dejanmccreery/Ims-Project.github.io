62481b716edea79bad1f056edd920a27941bddc9
JustaGlitch <justingraham1108@gmail.com>
2022-07-26 13:52:32 +0100

    Added items database

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -10,3 +10,12 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `price` DECIMAL(15,2) NOT NULL,
+    `stock` INT(11) NOT NULL,
+    PRIMARY KEY (`id`)
+);
