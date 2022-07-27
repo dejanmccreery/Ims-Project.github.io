4fe3faaf292c62746411e296b9d09c7cc35cd505
JustaGlitch <justingraham1108@gmail.com>
2022-07-27 20:09:28 +0100

    added order controller

---
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -0,0 +1,56 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.OrderDAO;
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.Utils;
+
+public class OrderController implements CrudController<Order> {
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    private OrderDAO orderDAO;
+    private Utils utils;
+
+    public OrderController(OrderDAO orderDAO, Utils utils) {
+        super();
+        this.orderDAO = orderDAO;
+        this.utils = utils;
+    }
+
+
+    @Override
+    public List<Order> readAll() {
+        List<Order> orders = orderDAO.readAll();
+        for (Order order: orders) {
+            LOGGER.info(order);
+        }
+        return orders;
+    }
+
+    @Override
+    public Order create() {
+		LOGGER.info("Please enter a customer ID");
+		Long customerID = utils.getLong();
+		Order order = orderDAO.create(new Order(customerID));
+		LOGGER.info("Order created");
+		return order;
+    }
+
+
+    @Override
+    public int delete() {
+		LOGGER.info("Please enter the id of the order you would like to delete");
+		Long id = utils.getLong();
+		return orderDAO.delete(id);
+
+    }
+
+    public Order update() {
+        return null;
+    }
+}
